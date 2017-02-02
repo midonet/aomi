670b85fccba5e91cfda4552544272209eed2ff43
Abel Boldú <abel.boldu@gmx.com>
2017-02-02 15:34:03 +0100

    Change repo, use output

--- scripts/install-ansible.sh
+++ scripts/install-ansible.sh
@@ -3,14 +3,14 @@
 APT=`command -v apt-get` || true
 YUM=`command -v yum` || true
 if [[ "$APT" != "" ]]; then
-  sudo apt-get update >> /dev/null
-  sudo apt-get install -qqy software-properties-common sshpass >> /dev/null
-  sudo apt-add-repository -y ppa:ansible/ansible >> /dev/null
-  sudo apt-get update >> /dev/null
-  sudo apt-get install -qqy ansible >> /dev/null
+  sudo apt-get update
+  sudo apt-get install -qqy software-properties-common sshpass
+  sudo apt-add-repository -y ppa:ansible/ansible
+  sudo apt-get update
+  sudo apt-get install -qqy ansible
 elif [[ "$YUM" != "" ]]; then
-  sudo yum -y install epel-release >> /dev/null
-  sudo yum -y install git wget sshpass ansible >> /dev/null
+  sudo yum -y install epel-release
+  sudo yum -y install git wget sshpass ansible
 else
   echo "Distro unsupported."
   exit 1
